03dc7018e8d1a50777c3d62092b6e4b05e1555ee
rui <rui1105@outlook.com>
2020-04-26 15:43:18 +0800

    cpp_004_boost

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<boost/lexical_cast.hpp>
 
 using std::cout;
 using std::endl;
@@ -6,6 +7,13 @@ using std::cin;
 
 int main()
 {
+    using boost::lexical_cast;
+
+    int a= lexical_cast<int>("123");
+    double b = lexical_cast<double>("1.23");
+    
+    cout << a << endl;
+    cout << b << endl;
     cout << "123" << endl;
     return 0;
 }
